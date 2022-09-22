1ebdbd2d3c48a47c512643a250ba3e3a41f6ec0c
Kacper <kozadevpytania@gmail.com>
2022-09-22 10:08:01 +0200

    update db config

---
+++ config/env/production/database.js
@@ -0,0 +1,19 @@
+const parse = requier("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+
+module.export = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      debug: false,
+    },
+  },
+});
